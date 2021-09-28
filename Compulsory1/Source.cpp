6c25285634dc296b1f37e572fd65c22d6170bf4b
Christian Hagen Wiker <chrishawik@gmail.com>
2021-09-28 12:41:11 +0200

    Final (unless i feel like making more comments)

--- Compulsory1/Source.cpp
+++ Compulsory1/Source.cpp
@@ -4,23 +4,21 @@
 #include <Windows.h>
 #include <vector>
 
-void game();
-void checkDraw();
-void drawBoard();
-void calcWinner();
+void game(); //Input, switches player, assigns player token.
+void checkDraw(); //Ends the game if board is filled.
+void drawBoard(); //Draws the board with loops.
+void calcWinner(); //Calculates the winner.
 
 int row{};
 int col{};
 std::vector <std::vector<char>> table = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
-//char table[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
 int choice;
 bool gameWon = false;
 int currentPlayer = 1;
-
+bool draw = false;
 
 void game()
 {
-	checkDraw();
 
 	if (currentPlayer == 1)
 	{
@@ -31,10 +29,10 @@ void game()
 		std::cout << "\nPlayer 2 [O] take your turn: ";
 	}
 
-	char ch = _getch();
+	char ch = _getch(); //Saves the players from having to press enter for better playability.
 	int i = (int)((char)ch - '0');
 
-	switch (i)
+	switch (i) //Numerical input for each slot in the vector.
 	{
 	case 1: row = 0, col = 0;
 		break;
@@ -59,7 +57,7 @@ void game()
 		break;
 	}
 
-	if (currentPlayer == 1) 
+	if (currentPlayer == 1) //Assigns a token to the players.
 	{
 		table[row][col] = 'X';
 	}
@@ -82,25 +80,26 @@ void game()
 
 void checkDraw()
 {
-	int counter;
-	bool draw = false;
+
+	
 
 	for (int i = 0; i < 3; i++) 
 	{
 		
 		for (int j = 0; j < 3; j++)
 		{
-			if (table[i][j] == 'X')
+			if (table[i][j] == 'X') //Checks for X chars in the vector.
 			{
 				draw = true;
 				continue;
 			}
-			else if (table[i][j] == 'O') 
+			else if (table[i][j] == 'O') //Checks for O chars in the vector.
 			{
 				draw = true;
 				continue;
 			}
-			else {
+			else //The board is not filled and the main loop moves to next players turn.
+			{ 
 				draw = false;
 				break;
 			}
@@ -117,7 +116,7 @@ void checkDraw()
 
 void drawBoard()
 {
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 3; i++) //Draws the board.
 	{
 		std::cout << "-------------" << std::endl;
 		std::cout << "| ";
@@ -135,20 +134,20 @@ void calcWinner()
 {
 	for (int i=0; i < 3; i++)
 	{
-		if (table[i][0] == table[i][1] && table[i][1] == table[i][2])
+		if (table[i][0] == table[i][1] && table[i][1] == table[i][2]) //Checks if a player has won horizontally.
 		{
-			if (table[i][0] == 'X')
+			if (table[i][0] == 'X') //Checks which player has won.
 				currentPlayer = 1;
 			else
 				currentPlayer = 2;
 
 			gameWon = true;
-			system("cls");
-			drawBoard();
-			std::cout << "Player " << currentPlayer << " wins!!" << std::endl;
+			system("cls"); //Clears the screen.
+			drawBoard(); //Draws the new updated board.
+			std::cout << "Player " << currentPlayer << " wins!!" << std::endl; //Announces the winner.
 
 		}
-		if (table[0][i] == table[1][i] && table[1][i] == table[2][i])
+		if (table[0][i] == table[1][i] && table[1][i] == table[2][i]) //Checks if a player has won vertically.
 		{
 			if (table[0][i] == 'X')
 				currentPlayer = 1;
@@ -161,7 +160,7 @@ void calcWinner()
 			std::cout << "Player " << currentPlayer << " wins!!" << std::endl;
 
 		}
-		if (table[0][0] == table[1][1] && table[1][1] == table[2][2])
+		if (table[0][0] == table[1][1] && table[1][1] == table[2][2]) //Checks diagonal 1.
 		{
 			if (table[0][0] == 'X')
 				currentPlayer = 1;
@@ -174,7 +173,7 @@ void calcWinner()
 			std::cout << "Player " << currentPlayer << " wins!!" << std::endl;
 
 		}
-		if (table[0][2] == table[1][1] && table[1][1] == table[2][0])
+		if (table[0][2] == table[1][1] && table[1][1] == table[2][0]) //Checks diagonal 2.
 		{
 			if (table[0][2] == 'X')
 				currentPlayer = 1;
@@ -194,22 +193,25 @@ void calcWinner()
 int main()
 {
 	
-	char answer{ 'y' };
-
+	char answer{ 'y' }; //Makes the game run the first time.
 	
-	while (tolower(answer) == 'y') 
+	while (tolower(answer) == 'y') //Loops the game while the answer is y.
 	{
+
+		currentPlayer = 1; //Makes sure it's player 1's turn every new game.
+
 		std::cout << "Welcome to Tic Tac Toe!\n\n";
 		system("pause");
 		gameWon = false;
-		table = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
+		table = { {'1','2','3'},{'4','5','6'},{'7','8','9'} }; //Resets the board to it's original state.
 		
 		while (gameWon == false) 
 		{
 			system("cls");
 			drawBoard();
+			checkDraw();
 			calcWinner();
-			if (gameWon == true)
+			if (gameWon == true) //Shows the correct winner (messy hotfix).
 			{
 				if (currentPlayer == 1)
 				{
@@ -221,18 +223,16 @@ int main()
 				}
 				break;
 			}
+			else if (draw == true) //Ends the game if it's a draw.
+			{
+				std::cout << std::endl;
+				break;
+			}
 			game();
 		}
 		std::cout << "Do you want to play again? y/n: ";
 		std::cin >> answer;
-		/*if (answer == 'y') {
-			gameWon = false;
-			return 0;
-		}
-		else if (tolower(answer) == 'n')
-		{
-			exit(0);
-		}*/
+	
 	}
 	
 }
